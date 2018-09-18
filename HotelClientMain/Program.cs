89b2da38cb69615f7a5d3274e623b16a28333e70
Pallvi Goel <pgoel@tavisca.com>
2018-09-18 18:44:58 +0530

    WCF Project Hotel Added

---
+++ HotelClientMain/Program.cs
@@ -0,0 +1,40 @@
+﻿using HotelClientMain.HotelServiceReference;
+using HotelData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelClientMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HotelClient hotelClient = new HotelClient("BasicHttpBinding_IHotel");
+            List<HotelServiceReference.HotelModel> hotels;
+            
+            hotels = hotelClient.GetAllHotels().ToList();
+            foreach (var res in hotels)
+            {
+                Console.WriteLine(res.Id);
+                Console.WriteLine(res.Name);
+                Console.WriteLine(res.Location);
+                Console.WriteLine(res.HotelRating);
+                Console.WriteLine(" ");
+
+            }
+
+            int n = Int32.Parse(Console.ReadLine());
+
+            HotelServiceReference.HotelModel hotel = hotelClient.GetHotelById(n);
+            Console.WriteLine(hotel.Id);
+            Console.WriteLine(hotel.Name);
+            Console.WriteLine(hotel.Location);
+            Console.WriteLine(hotel.HotelRating);
+            Console.WriteLine(" ");
+            Console.ReadKey();
+        }
+    }
+    }
